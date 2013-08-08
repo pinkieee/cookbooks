fd30c1315f55b6ac6f1d9aaa26f92bd901d9baa1
unknown <yvonne.beumer@cloudreach.co.uk>
2013-08-08 11:12:14 +0000

    Removed accidental option

--- windows/recipes/wutask.rb
+++ windows/recipes/wutask.rb
@@ -12,7 +12,7 @@ windows_task "critupdate" do
   user "Administrator"
   password "*********"
   cwd "C:/windows/system32/windowspowershell/v1.0/"
-  command "powershell.exe -L C:/critwindowsupdate.ps1"
+  command "powershell.exe C:/critwindowsupdate.ps1"
   run_level :highest
   frequency :minute
   frequency_modifier 15
@@ -22,7 +22,7 @@ windows_task "installcritreboot" do
   user "Administrator"
   password "*********"
   cwd "C:/windows/system32/windowspowershell/v1.0/"
-  command "powershell.exe -L C:/installcritreboot.ps1"
+  command "powershell.exe C:/installcritreboot.ps1"
   run_level :highest
   frequency :minute
   frequency_modifier 15
